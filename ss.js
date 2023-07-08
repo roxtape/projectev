bc98951dfbb52c4d51181d4a29e01948152ffa27
roxtape <refizaderamiz97@gmail.com>
2023-07-08 10:32:07 +0300

    ndng

---
+++ ss.js
@@ -0,0 +1,10 @@
+let ucbuca = alert('Duzbucaqlinin Sahesini tapan Proqram');
+let a = +prompt ('Duzbucaqlinin A terefini Qeyd Edin');
+let b = +prompt ('Duzbucaqlinin B terefini Qeyd Edin');
+
+
+let duzbucaqpr = a * b  ;
+
+document.getElementById('ramo').innerHTML = 'A terefi '+ a + ' sm' + ' B terefi ise '+ b + ' sm olan duzbucaqin sahesi '+  duzbucaqpr+ ' sm olar';
+
+
